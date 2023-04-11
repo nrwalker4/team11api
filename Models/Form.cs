53370bdf5a45ad9759df3240b937fe1a9c39d796
Kincaid Keating <kincaidkeating@MacBook-Pro-3.local>
2023-04-11 18:04:35 -0500

    Models

---
+++ Models/Form.cs
@@ -0,0 +1,13 @@
+namespace team11api.Models
+{
+    public class Form
+    {
+        public int FormId{get;set;}
+        public string IndoorOutdoor{get;set;}
+        public string SunExposure{get;set;}
+        public string Soil{get;set;}
+        public Form(){
+            
+        }
+    }
+}
---
+++ Models/OLI.cs
@@ -0,0 +1,15 @@
+namespace team11api.Models
+{
+    public class OLI
+    {
+        public int OLId{get;set;}
+        public int ItemQty{get;set;}
+        public decimal UnitPrice{get;set;}
+        public int OrderId{get;set;}
+        public OLI(){
+            
+        }
+
+        //subtotal
+    }
+}
